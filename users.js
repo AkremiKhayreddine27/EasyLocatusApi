b99a2fd3ee2b4bf00a8277fe1961b01f234d4d5e
khayreddine <khayreddine27@gmail.com>
2018-03-26 09:43:33 +0100

    First Commit

---
+++ users.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+const users = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@easylocatus.com',
+    password: '123456',
+    birthDay: new Date(),
+    phone: '0021625268368'
+  }
+];
+
+module.exports = users;
