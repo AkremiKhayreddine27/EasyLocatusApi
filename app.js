53403cfca86d38b89283ff0c8458d5286ab80a98
khayreddine <khayreddine27@gmail.com>
2018-03-26 12:27:28 +0100

    First Commit

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/api/user', auth.authenticate(), function (req, res) {
+  console.log(auth.authenticate());
   res.json({
     data: users[req.user.id]
   });
